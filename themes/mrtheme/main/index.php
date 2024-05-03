c184a676c376e5cb6124bc2bedef4d6bc857b24d
Artur Vale <47109035+ArturllVale@users.noreply.github.com>
2024-05-03 16:41:43 -0300

    Uploda

--- themes/mrtheme/main/index.php
+++ themes/mrtheme/main/index.php
@@ -2,13 +2,14 @@
 
 <div class="container">		
 <div class="row">
-<div class="col-md-6">
+<div class="col-md-3">
 <div class="card" style="width: 18rem;">
   <img src="https://i.imgur.com/TFTQ1bG.jpeg" class="card-img-top" alt="...">
   <div class="card-body">
     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
   </div>
 </div>
+<div class="col-md-3">
 <div class="card" style="width: 18rem;">
   <img src="https://i.imgur.com/TFTQ1bG.jpeg" class="card-img-top" alt="...">
   <div class="card-body">
@@ -16,8 +17,15 @@
   </div>
 </div>
 </div>
-<div class="col-md-6">
-
+</div>
+<div class="col-md-3">
+<div class="card" style="width: 18rem;">
+  <img src="https://i.imgur.com/TFTQ1bG.jpeg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  </div>
+</div>
+</div>
 </div>
 </div>
 </div>
