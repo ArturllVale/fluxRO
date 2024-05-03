20de2575f13a4275a189c84bac0b8219ca1c159d
Artur Vale <47109035+ArturllVale@users.noreply.github.com>
2024-05-03 16:44:27 -0300

    Update

--- themes/mrtheme/main/index.php
+++ themes/mrtheme/main/index.php
@@ -18,13 +18,9 @@
                 </div>
             </div>
         </div>
-        <div class="col-md-3">
-            <div class="card">
-                <img src="https://i.imgur.com/TFTQ1bG.jpeg" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                </div>
-            </div>
+        <div class="col-md-6">
+            <h2>FluxRO</h2>
+			<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat ducimus dolor, explicabo est nisi, quos iste aliquam culpa accusamus minima expedita libero, vero in sequi eligendi eius illo distinctio ad.</p>
         </div>
     </div>
 </div>
