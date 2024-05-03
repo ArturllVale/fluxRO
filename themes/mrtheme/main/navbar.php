a3ccf15e733e93c8bc2a70df3387b4a5a732c067
Artur Vale <47109035+ArturllVale@users.noreply.github.com>
2024-05-03 10:42:39 -0300

    Update Nome do Site

--- themes/mrtheme/main/navbar.php
+++ themes/mrtheme/main/navbar.php
@@ -2,7 +2,8 @@
 
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
 	<div class="container-fluid">
-		<a class="navbar-brand" href="./"><?php echo Flux::config('SiteTitle'); ?></a>
+		<!-- <a class="navbar-brand" href="./"><?php echo Flux::config('SiteTitle'); ?></a> -->
+		<a class="navbar-brand" href="./">Nome do Site</a>
 		<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
 			<span class="navbar-toggler-icon"></span>
 		</button>
