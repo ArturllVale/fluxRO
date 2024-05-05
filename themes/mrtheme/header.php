8d3b95ed4fe56050e569a9d69437b8fb77599926
Artur Vale <artursantosvale@gmail.com>
2024-05-04 23:02:18 -0300

    Update header.php

--- themes/mrtheme/header.php
+++ themes/mrtheme/header.php
@@ -41,44 +41,48 @@
 			<header class="container">
 				<div class="row">
 					<div class="col-md-4">
+					<div class="content">
+  <h1 class="heading">Card Flip</h1>
+  <p class="description">Hover over a card to flip it.</p>
+  <a class="card" href="#!">
+    <div class="front" style="background-image: url(//source.unsplash.com/300x401)">
+      <p>Lorem ipsum dolor sit amet consectetur adipisi.</p>
+    </div>
+    <div class="back">
+      <div>
+        <p>Consectetur adipisicing elit. Possimus, praesentium?</p>
+        <p>Provident consectetur natus voluptatem quis tenetur sed beatae eius sint.</p>
+        <button class="button">Click Here</button>
+      </div>
+    </div>
+  </a>
+  <a class="card" href="#!">
+    <div class="front" style="background-image: url(//source.unsplash.com/300x402)">
+      <p>Lorem ipsum dolor sit amet consectetur adipisi.</p>
+    </div>
+    <div class="back">
+      <div>
+        <p>Consectetur adipisicing elit. Possimus, praesentium?</p>
+        <p>Provident consectetur natus voluptatem quis tenetur sed beatae eius sint.</p>
+        <button class="button">Click Here</button>
+      </div>
+    </div>
+  </a>
+  <a class="card" href="#!">
+    <div class="front" style="background-image: url(//source.unsplash.com/300x403)">
+      <p>Lorem ipsum dolor sit amet consectetur adipisi.</p>
+    </div>
+    <div class="back">
+      <div>
+        <p>Consectetur adipisicing elit. Possimus, praesentium?</p>
+        <p>Provident consectetur natus voluptatem quis tenetur sed beatae eius sint.</p>
+        <button class="button">Click Here</button>
+      </div>
+    </div>
+  </a>
+</div>
 
-						<div class="content">
-							
-								<div class="front" style="background-image: url(//source.unsplash.com/300x401)">
-									<p>Lorem ipsum dolor sit amet consectetur adipisi.</p>
-								</div>
-								<div class="back">
-									<div>
-										<p>Consectetur adipisicing elit. Possimus, praesentium?</p>
-										<p>Provident consectetur natus voluptatem quis tenetur sed beatae eius sint.</p>
-										<button class="button">Click Here</button>
-									</div>
-								</div>
-							</a><a class="card" href="#!">
-								<div class="front" style="background-image: url(//source.unsplash.com/300x402)">
-									<p>Lorem ipsum dolor sit amet consectetur adipisi.</p>
-								</div>
-								<div class="back">
-									<div>
-										<p>Consectetur adipisicing elit. Possimus, praesentium?</p>
-										<p>Provident consectetur natus voluptatem quis tenetur sed beatae eius sint.</p>
-										<button class="button">Click Here</button>
-									</div>
-								</div>
-							</a><a class="card" href="#!">
-								<div class="front" style="background-image: url(//source.unsplash.com/300x403)">
-									<p>Lorem ipsum dolor sit amet consectetur adipisi.</p>
-								</div>
-								<div class="back">
-									<div>
-										<p>Consectetur adipisicing elit. Possimus, praesentium?</p>
-										<p>Provident consectetur natus voluptatem quis tenetur sed beatae eius sint.</p>
-										<button class="button">Click Here</button>
-									</div>
-								</div>
-							</a>
-						</div>
-					</div>
+					</div>	
 					<div class="col-md-4 center">
 						<h1 class="mrtitle">Junte-se a mais de <span class="mrnumber">1.248</span> Jogadores online e
 							venha
