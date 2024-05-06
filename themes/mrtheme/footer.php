3e0da10e6b34d933dee2b6108a6218b18a96940b
Artur Vale <47109035+ArturllVale@users.noreply.github.com>
2024-05-06 10:34:40 -0300

    Update footer

--- themes/mrtheme/footer.php
+++ themes/mrtheme/footer.php
@@ -1,6 +1,23 @@
 <?php if (!defined('FLUX_ROOT')) exit; ?>
 
 	</div>
+<footer class="container py-4">
+	<div class="row">
+		<!-- Ícones das redes sociais -->
+		<div class="col">
+			<a href="#" class="text-decoration-none"><i class="fab fa-facebook social-icon"></i></a>
+			<a href="#" class="text-decoration-none"><i class="fab fa-instagram social-icon"></i></a>
+			<a href="#" class="text-decoration-none"><i class="fab fa-tiktok social-icon"></i></a>
+			<a href="#" class="text-decoration-none"><i class="fab fa-discord social-icon"></i></a>
+		</div>
+	</div>
+	<div class="row mt-3">
+		<!-- Texto do rodapé -->
+		<div class="col">
+			Feito com <i class="fas fa-heart text-danger"></i> e Bootstrap 5 | MundoRag 2024-2025 &copy;
+		</div>
+	</div>
+</footer>
 	<!-- <div id="footer">
 		<div class="container">
 			<p class="text-muted">
@@ -144,5 +161,7 @@
 		</script>
 
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+	<!-- Font Awesome -->
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/js/all.min.js"></script>
 	</body>
 </html>
