f4095eb5be89d85ee9f0c15bcea39b6503712f7b
Artur Vale <47109035+ArturllVale@users.noreply.github.com>
2024-05-14 10:38:36 -0300

    Update

--- themes/mrtheme/main/submenu.php
+++ themes/mrtheme/main/submenu.php
@@ -2,31 +2,27 @@
 
 <?php 
     $subMenuItems = $this->getSubMenuItems(); 
-    $menus = array(); 
+    $breadcrumbs = array(); 
 ?>
 
 <?php if (!empty($subMenuItems)): ?>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container-fluid">
-            <span class="navbar-text">Menu:</span>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <?php foreach ($subMenuItems as $menuItem): ?>
-                        <?php 
-                            $isActive = $params->get('module') == $menuItem['module'] && $params->get('action') == $menuItem['action']; 
-                            $menuClass = $isActive ? 'nav-item active' : 'nav-item'; 
-                        ?>
-                        <li class="<?php echo $menuClass; ?>">
-                            <a class="nav-link" href="<?php echo $this->url($menuItem['module'], $menuItem['action']); ?>">
-                                <?php echo htmlspecialchars($menuItem['name']); ?>
-                                <?php if ($isActive): ?>
-                                    <span class="visually-hidden">(current)</span>
-                                <?php endif; ?>
-                            </a>
-                        </li>
-                    <?php endforeach; ?>
-                </ul>
-            </div>
-        </div>
+    <nav aria-label="breadcrumb">
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item"><a href="<?php echo $this->url('index', 'index'); ?>">Home</a></li>
+            <?php foreach ($subMenuItems as $index => $menuItem): ?>
+                <?php 
+                    $isActive = $params->get('module') == $menuItem['module'] && $params->get('action') == $menuItem['action']; 
+                ?>
+                <li class="breadcrumb-item <?php echo $isActive ? 'active' : ''; ?>" aria-current="<?php echo $isActive ? 'page' : ''; ?>">
+                    <?php if (!$isActive): ?>
+                        <a href="<?php echo $this->url($menuItem['module'], $menuItem['action']); ?>">
+                            <?php echo htmlspecialchars($menuItem['name']); ?>
+                        </a>
+                    <?php else: ?>
+                        <?php echo htmlspecialchars($menuItem['name']); ?>
+                    <?php endif; ?>
+                </li>
+            <?php endforeach; ?>
+        </ol>
     </nav>
 <?php endif; ?>
