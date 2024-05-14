59e9ce8229e7d0b29e9021ab6593277b20243127
Artur Vale <47109035+ArturllVale@users.noreply.github.com>
2024-05-14 16:21:09 -0300

    php

--- themes/mrtheme/account/resend.php
+++ themes/mrtheme/account/resend.php
@@ -1,37 +1,42 @@
 <?php if (!defined('FLUX_ROOT')) exit; ?>
-<h2><?php echo htmlspecialchars(Flux::message('ResendHeading')) ?></h2>
-<?php if (!empty($errorMessage)): ?>
-<p class="red"><?php echo htmlspecialchars($errorMessage) ?></p>
-<?php endif ?>
-<p><?php echo htmlspecialchars(Flux::message('ResendInfo')) ?></p>
-<form action="<?php echo $this->urlWithQs ?>" method="post" class="generic-form">
-	<table class="generic-form-table">
-		<?php if (count($serverNames) > 1): ?>
-		<tr>
-			<th><label for="login"><?php echo htmlspecialchars(Flux::message('ResendServerLabel')) ?></label></th>
-			<td>
-				<select name="login" id="login"<?php if (count($serverNames) === 1) echo ' disabled="disabled"' ?>>
-				<?php foreach ($serverNames as $serverName): ?>
-					<option value="<?php echo htmlspecialchars($serverName) ?>"<?php if ($params->get('server') == $serverName) echo ' selected="selected"' ?>><?php echo htmlspecialchars($serverName) ?></option>
-				<?php endforeach ?>
-				</select>
-			</td>
-			<td><p><?php echo htmlspecialchars(Flux::message('ResendServerInfo')) ?></p></td>
-		</tr>
-		<?php endif ?>
-		<tr>
-			<th><label for="userid"><?php echo htmlspecialchars(Flux::message('ResendAccountLabel')) ?></label></th>
-			<td><input type="text" name="userid" id="userid" /></td>
-			<td><p><?php echo htmlspecialchars(Flux::message('ResendAccountInfo')) ?></p></td>
-		</tr>
-		<tr>
-			<th><label for="email"><?php echo htmlspecialchars(Flux::message('ResendEmailLabel')) ?></label></th>
-			<td><input type="text" name="email" id="email" /></td>
-			<td><p><?php echo htmlspecialchars(Flux::message('ResendEmailInfo')) ?></p></td>
-		</tr>
-		<tr>
-			<td colspan="2" align="right"><input type="submit" value="<?php echo htmlspecialchars(Flux::message('ResendButton')) ?>" /></td>
-			<td></td>
-		</tr>
-	</table>
-</form>
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-6">
+            <div class="card mt-5">
+                <div class="card-body">
+                    <h2 class="card-title"><?php echo htmlspecialchars(Flux::message('ResendHeading')) ?></h2>
+                    <?php if (!empty($errorMessage)): ?>
+                        <p class="text-danger"><?php echo htmlspecialchars($errorMessage) ?></p>
+                    <?php endif ?>
+                    <p><?php echo htmlspecialchars(Flux::message('ResendInfo')) ?></p>
+                    <form action="<?php echo $this->urlWithQs ?>" method="post" class="generic-form">
+                        <div class="mb-3">
+                            <?php if (count($serverNames) > 1): ?>
+                                <label for="login" class="form-label"><?php echo htmlspecialchars(Flux::message('ResendServerLabel')) ?></label>
+                                <select name="login" id="login" class="form-select"<?php if (count($serverNames) === 1) echo ' disabled' ?>>
+                                    <?php foreach ($serverNames as $serverName): ?>
+                                        <option value="<?php echo htmlspecialchars($serverName) ?>"<?php if ($params->get('server') == $serverName) echo ' selected' ?>><?php echo htmlspecialchars($serverName) ?></option>
+                                    <?php endforeach ?>
+                                </select>
+                                <p class="form-text"><?php echo htmlspecialchars(Flux::message('ResendServerInfo')) ?></p>
+                            <?php endif ?>
+                        </div>
+                        <div class="mb-3">
+                            <label for="userid" class="form-label"><?php echo htmlspecialchars(Flux::message('ResendAccountLabel')) ?></label>
+                            <input type="text" name="userid" id="userid" class="form-control">
+                            <p class="form-text"><?php echo htmlspecialchars(Flux::message('ResendAccountInfo')) ?></p>
+                        </div>
+                        <div class="mb-3">
+                            <label for="email" class="form-label"><?php echo htmlspecialchars(Flux::message('ResendEmailLabel')) ?></label>
+                            <input type="text" name="email" id="email" class="form-control">
+                            <p class="form-text"><?php echo htmlspecialchars(Flux::message('ResendEmailInfo')) ?></p>
+                        </div>
+                        <div class="mb-3">
+                            <button type="submit" class="btn btn-primary"><?php echo htmlspecialchars(Flux::message('ResendButton')) ?></button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
